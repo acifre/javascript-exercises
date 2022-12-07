c0d636bf46844d9ba46754e1a3efcdc5404127a3
acifre <78503928+acifre@users.noreply.github.com>
2022-12-06 19:13:05 -0600

    Complete part of exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, remove) {
+    index = array.indexOf(remove);
+    if (index > -1) {
+        newArray = array.splice(index, 1);
+    }
+    
+    return array;
 };
+
+// for mutliple arguments well need to use a for loop probably
 
 // Do not edit below this line
 module.exports = removeFromArray;
