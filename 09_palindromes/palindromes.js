1eb5438faad41db2471a182010d6c87a5e7d736f
acifre <78503928+acifre@users.noreply.github.com>
2022-12-10 13:49:11 -0600

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    newString = string
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+        .replace(/\s{2,}/g," ").replace(/\s/g, '')
+        .toLowerCase()
+        .trim();
+
+    palindrome = "";
+
+    for (let i = newString.length - 1; i >= 0; i--) {
+        palindrome += newString[i];
+    }
+
+    return newString === palindrome;
+
+
 
 };
 
