8ad078f75688421a227ce5cd7a3867b495162fe0
acifre <78503928+acifre@users.noreply.github.com>
2022-12-10 13:24:12 -0600

    Finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,51 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  if (num2 > num1) {
+    return num2 - num1;
+  } else {
+    return num1 - num2;
+  }
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+
+  sumArray = array.reduce((sum, number) => {
+    sum += number;
+    return sum;
+  }, 0);
+
+  return sumArray;
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+
+  multiplyArray = array.reduce((multiply, number) => {
+    multiply *= number;
+    return multiply;
+  }, 1);
+
+  return multiplyArray;
 
 };
 
-const power = function() {
+const power = function(number, power) {
+  return number**power;
 	
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  fact = 1;
+  for (let i = number; i > 0; i--){
+    fact *= i;
+  }
+
+  return fact;
 	
 };
 
