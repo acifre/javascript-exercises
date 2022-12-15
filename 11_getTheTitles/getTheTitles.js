8d7a9982e6852aee89152ab0acbc75afc24e5e5a
acifre <78503928+acifre@users.noreply.github.com>
2022-12-15 17:29:15 -0600

    Finish getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    titleArray = [];
+
+    for (let book of books) {
+        titleArray.push(book.title);
+    }
+
+    return titleArray;
 };
 
 // Do not edit below this line
